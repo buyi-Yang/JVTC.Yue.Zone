6693c7454164c9e4a9dfeb441013899b255fe561
Yue-plus <Yue_plus@foxmail.com>
2022-09-08 01:14:40 +0800

    fix TestMethodOrder

--- ComprehensiveExerciseQuestions/src/test/java/zone/yue/jvtc/solution/ceq/Unit1Test.java
+++ ComprehensiveExerciseQuestions/src/test/java/zone/yue/jvtc/solution/ceq/Unit1Test.java
@@ -1,54 +1,67 @@
 package zone.yue.jvtc.solution.ceq;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-public class Unit1Test {
+@TestMethodOrder(MethodOrderer.MethodName.class)
+class Unit1Test {
+    @BeforeAll
+    static void testU1Before() {
+        System.out.println("----- 单元一 公式计算 测试开始 -----");
+    }
+
     @Test
-    public void testTriangleArea() {
+    void testU1Q1TriangleArea() {
         double a = 5.0;
         double h = 3.0;
-        System.out.printf("Q1 - 底边为 %g 高为 %g 的三角形面积为 %g%n", a, h, Unit1.triangleArea(a, h));
+        System.out.printf("U1Q1 - 底边为 %g 高为 %g 的三角形面积为 %g%n", a, h, Unit1.triangleArea(a, h));
     }
 
     @Test
-    public void testParallelogramPerimeter() {
+    void testU1Q2ParallelogramPerimeter() {
         double a = 1.0;
         double b = 2.0;
-        System.out.printf("Q2 - 边长分别为 %g 与 %g 的平行四边形周长为 %g%n", a, b, Unit1.parallelogramPerimeter(a, b));
+        System.out.printf("U1Q2 - 边长分别为 %g 与 %g 的平行四边形周长为 %g%n", a, b, Unit1.parallelogramPerimeter(a, b));
     }
 
     @Test
-    public void testCuboidVolume() {
+    void testU1Q3CuboidVolume() {
         double a = 2.0;
         double b = 3.0;
         double h = 5.0;
-        System.out.printf("Q3 - 长为 %g 宽为 %g 高为 %g 的长方体体积为 %g%n", a, b, h, Unit1.cuboidVolume(a, b, h));
+        System.out.printf("U1Q3 - 长为 %g 宽为 %g 高为 %g 的长方体体积为 %g%n", a, b, h, Unit1.cuboidVolume(a, b, h));
     }
 
     @Test
-    public void testCylinderVolume() {
+    void testU1Q4CylinderVolume() {
         double r = 3.0;
         double h = 5.0;
-        System.out.printf("Q4 - 半径为 %g 高为 %g 的圆柱体的体积为 %g%n", r, h, Unit1.cylinderVolume(r, h));
+        System.out.printf("U1Q4 - 半径为 %g 高为 %g 的圆柱体的体积为 %g%n", r, h, Unit1.cylinderVolume(r, h));
     }
 
     @Test
-    public void testConeVolume() {
+    void testU1Q5ConeVolume() {
         double r = 3.0;
         double h = 5.0;
-        System.out.printf("Q5 - 半径为 %g 高为 %g 的圆锥体的体积为 %g%n", r, h, Unit1.coneVolume(r, h));
+        System.out.printf("U1Q5 - 半径为 %g 高为 %g 的圆锥体的体积为 %g%n", r, h, Unit1.coneVolume(r, h));
     }
 
     @Test
-    public void testCircuitPower() {
+    void testU1Q6CircuitPower() {
         double u = 3.0;
         double r = 5.0;
-        System.out.printf("Q6 - 电压为 %g 电阻为 %g 的电路的电功率为 %g%n", u, r, Unit1.resistanceCircuitPower(u, r));
+        System.out.printf("U1Q6 - 电压为 %g 电阻为 %g 的电路的电功率为 %g%n", u, r, Unit1.resistanceCircuitPower(u, r));
     }
 
-    @Test public void testNotResistanceCircuitPower() {
+    @Test
+    void testU1Q7NotResistanceCircuitPower() {
         double i = 3.0;
         double r = 5.0;
-        System.out.printf("Q7 - 电流为 %g 电阻为 %g 的电路的电热为 %g%n", i, r, Unit1.notResistanceCircuitPower(i, r));
+        System.out.printf("U1Q7 - 电流为 %g 电阻为 %g 的电路的电热为 %g%n", i, r, Unit1.notResistanceCircuitPower(i, r));
+    }
+
+
+    @AfterAll
+    static void testU1AfterAll() {
+        System.out.println("===== 单元一 公式计算 测试结束 =====\n");
     }
 }
--- ComprehensiveExerciseQuestions/src/test/java/zone/yue/jvtc/solution/ceq/Unit3Test.java
+++ ComprehensiveExerciseQuestions/src/test/java/zone/yue/jvtc/solution/ceq/Unit3Test.java
@@ -1,24 +1,30 @@
 package zone.yue.jvtc.solution.ceq;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-public class Unit3Test {
+@TestMethodOrder(MethodOrderer.MethodName.class)
+class Unit3Test {
+    @BeforeAll
+    static void testU3BeforeAll() {
+        System.out.println("----- 单元三 基本循环（要求用 while 语句设计） 测试开始 -----");
+    }
+
     @Test
-    public void testPassJudgment() {
+    void testU3Q1PassJudgment() {
         // 此示例共 10 名学生，其中 4 人及格，6 人不及格。
         double[] list = {0, 1, 20, 30, 50, 59, 60, 80.5, 99, 100};
         System.out.println("U3Q1 - " + Unit3.passJudgment(list));
     }
 
     @Test
-    public void testHighestAndLowestScore() {
+    void testU3Q2HighestAndLowestScore() {
         // 此示例共 10 名学生，其中最高分为 100 分，最低分为 -99 分。
         double[] list = {50, 59, 60, 80.5, 0, 1, 20, 30, -99, 100};
         System.out.println("U3Q2 - " + Unit3.highestAndLowestScore(list));
     }
 
     @Test
-    public void testCountAlphabetAndOther() {
+    void testU3Q3CountAlphabetAndOther() {
         // 此句长度为 44，其中有 35 个字母（a~z 与 A~Z）。
         String en = "The quick brown fox jumps over the lazy dog.";
         // 输入中文会导致异常结果，当前认为是编码转换导致的。
@@ -28,7 +34,7 @@ public class Unit3Test {
     }
 
     @Test
-    public void testVoteStatistics() {
+    void testU3Q4VoteStatistics() {
         // 此列表中张同学 5 票，王同学 3 票，无效票 2 张。
         String[] zhangWin = {"张同学", "张同学", "王同学", "张同学", "张同学", "王同学", "王同学", "张同学", "张三", "狗蛋"};
         String[] even = {"张同学", "王同学"};
@@ -38,18 +44,23 @@ public class Unit3Test {
     }
 
     @Test
-    public void testJuryScoring() {
+    void testU3Q5JuryScoring() {
         double[] list = {-0.1, 0, 15.9, 666, -233, 3.14, 77, 486, 114, 514, 5};
         System.out.println("U3Q5 - " + Unit3.juryScoring(list));
     }
 
     @Test
-    public void testCount() {
+    void testU3Q6Count() {
         System.out.println("U3Q6 - " + Unit3.count());
     }
 
     @Test
-    public void testLeapYear() {
+    void testU3Q7LeapYear() {
         System.out.println("U3Q7 - " + Unit3.leapYear());
+    }
+
+    @AfterAll
+    static void testU3AfterAll() {
+        System.out.println("===== 单元三 基本循环（要求用 while 语句设计） 测试结束 =====\n");
     }
 }
