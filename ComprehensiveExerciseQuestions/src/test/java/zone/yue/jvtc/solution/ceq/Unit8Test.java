7b51b64eb18e0b6c8ba8f05564a2b4aa797fb665
Yue-plus <Yue_plus@foxmail.com>
2022-09-15 15:26:58 +0800

    done Unit8 - Q2

--- ComprehensiveExerciseQuestions/src/test/java/zone/yue/jvtc/solution/ceq/Unit8Test.java
+++ ComprehensiveExerciseQuestions/src/test/java/zone/yue/jvtc/solution/ceq/Unit8Test.java
@@ -12,4 +12,11 @@ public class Unit8Test {
         double[] arr = {1, 2, 5, 10, 18, -66, -87, 0, 123, 0.0, 0.1, -0.1, -0.6666};
         System.out.println("U8Q1 - " + Unit8.q1(arr));
     }
+
+    @Test
+    void testU8Q2() {
+        // 优：2 人；良：3 人；中：2 人；及格：1 人；不及格：5 人；
+        double[] arr = {90, 91, 89.9999, 80.01, 81.999, 79.999, 70.0001, 61, 59.99999, 30, -31, 0.001, -0.999};
+        System.out.println("U8Q2 - " + Unit8.q2(arr));
+    }
 }
