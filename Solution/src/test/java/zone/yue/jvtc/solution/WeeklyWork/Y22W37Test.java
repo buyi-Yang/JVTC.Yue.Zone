7f22f5b8efae084c664ff5af9c7a91c9f3898d63
Yue-plus <Yue_plus@foxmail.com>
2022-10-07 20:13:35 +0800

    add Y22W37Test

---
+++ Solution/src/test/java/zone/yue/jvtc/solution/WeeklyWork/Y22W37Test.java
@@ -0,0 +1,19 @@
+package zone.yue.jvtc.solution.WeeklyWork;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class Y22W37Test {
+    @BeforeAll
+    static void testY22W37BeforeAll() {
+        System.out.println("----- 2022 年第 37 周编程作业 测试开始 -----");
+    }
+
+    @AfterAll
+    static void testY22W37AfterAll() {
+        System.out.println("===== 2022 年第 37 周编程作业 测试结束 =====");
+    }
+}
