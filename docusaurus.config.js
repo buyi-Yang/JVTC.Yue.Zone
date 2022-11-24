4129fc4edc9b9e857598102542b59520ba8b5e2a
Yue-plus <Yue_plus@foxmail.com>
2022-11-24 20:46:31 +0800

    fix editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,11 +30,11 @@ const config = {
           remarkPlugins: [math],
           rehypePlugins: [katex],
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/Yue-plus/JVTC.Yue.Zone/tree/main/docs/',
+          editUrl: 'https://github.com/Yue-plus/JVTC.Yue.Zone/tree/main/',
         },
         blog: {
           showReadingTime: true,
-          editUrl: 'https://github.com/Yue-plus/JVTC.Yue.Zone/tree/main/blog/',
+          editUrl: 'https://github.com/Yue-plus/JVTC.Yue.Zone/tree/main/',
         },
         theme: {customCss: require.resolve('./src/css/custom.css')},
       }),
