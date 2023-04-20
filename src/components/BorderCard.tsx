cacadc9e1dc85c1069ee270b39d131bc67ff996f
Yue-plus <Yue_plus@foxmail.com>
2023-04-20 21:06:42 +0800

    done 通用英语B2题库 作文

--- src/components/BorderCard.tsx
+++ src/components/BorderCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function BorderCard({title, children}) {
     return <div style={{border: "1px solid black", padding: "12px 18px", margin: "12px 0"}}>
-        {title ?? <h4 style={{textAlign: "center"}}>{title}</h4>}
+        {title ? <h4 style={{textAlign: "center"}}>{title}</h4> : null}
         {children}
     </div>
 }
