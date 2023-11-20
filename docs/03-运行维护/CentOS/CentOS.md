2383a1869338c2ed893d4565bd96ef0758281fa0
Yue-plus <Yue_plus@foxmail.com>
2023-11-20 16:59:44 +0800

    add 运行维护 CentOS

---
+++ docs/03-运行维护/CentOS/CentOS.md
@@ -0,0 +1,9 @@
+# CentOS
+
+:::info
+CentOS 项目是一个由社区驱动的免费软件，提供了丰富的基础平台，供开源社区使用。
+
+[CentOS Linux](https://www.centos.org/centos-linux/)
+| [CentOS Stream](https://www.centos.org/centos-stream/)
+::: 
+
--- src/components/OperationIndex.tsx
+++ src/components/OperationIndex.tsx
@@ -5,5 +5,6 @@ export function LinuxIndex() {
     return Index([
         {icon: "Debian.png", title: "Debian↗", text: "通用操作系统", href: "https://www.debian.org/index.zh-cn.html"},
         {icon: "Ubuntu.svg", title: "Ubuntu", text: "企业开源与 Linux", href: "/docs/运行维护/Ubuntu/"},
+        {icon: "CentOS.svg", title: "CentOS", text: "RedHat 社区企业操作系统", href: "/docs/运行维护/CentOS/"},
     ])
 }
